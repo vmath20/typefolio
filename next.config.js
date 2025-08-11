5fb9b3fea30e6516bf29c540c360bda63f94d4c6
Vaibhav Mishra <vaibhavrichmishra@gmail.com>
2025-08-11 01:24:09 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
     domains: ['gravatar.com', 'brandfetch.com', 'localhost'],
   },
